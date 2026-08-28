11bd1ff032ffa5114654b3b42b2a855fc707ad4e
Dilayehu Dessalegn <dilayehudessalegn@gmail.com>
2026-08-28 07:29:58 +0300

    Time: 67 ms (71.54%), Space: 111.2 MB (73.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> duplicate;
+        unordered_set<int> duplicate;
         for(size_t i=0; i<nums.size(); ++i){
             if(duplicate.find(nums[i]) != duplicate.end()){
                 return true;
             }
-            else duplicate[nums[i]] = i;
+            duplicate.insert(nums[i]);
         }
         return false;
     }
